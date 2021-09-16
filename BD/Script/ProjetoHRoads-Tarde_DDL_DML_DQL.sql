3d9e935c16099f47ba10cbcfe44f106b9df01a00
NAYARA NASCIMENTO <nayara.escudeiro@portalsesisp.org.br>
2021-09-16 16:42:03 -0300

    bd feito

--- BD/Script/ProjetoHRoads-Tarde_DDL_DML_DQL.sql
+++ BD/Script/ProjetoHRoads-Tarde_DDL_DML_DQL.sql
@@ -6,38 +6,54 @@ GO
 
 ------------------------------------------------------------DDL----------------------------------------------------------------
 
-CREATE TABLE CLASSE(
+CREATE TABLE classe(
 idClasse TINYINT PRIMARY KEY IDENTITY (1,1),
 nomeClasse VARCHAR (20)NOT NULL UNIQUE
 );
 GO
 
-CREATE TABLE TIPO_HABILIDADE(
+CREATE TABLE tipoUsuario(
+idTipoUsuario SMALLINT PRIMARY KEY IDENTITY (1,1),
+tipoUsuario VARCHAR (20) NOT NULL
+); 
+GO
+
+drop table tipoUsuario
+drop table usuario
+
+CREATE TABLE usuario(
+idUsuario SMALLINT PRIMARY KEY IDENTITY (1,1),
+nomeUsuario VARCHAR (50),
+email VARCHAR (200),
+senha VARCHAR (100),
+idTipoUsuario SMALLINT FOREIGN KEY REFERENCES tipoUsuario(idTipoUsuario)
+);
+GO
+
+
+CREATE TABLE tipoHabilidade(
 idTipoHabilidade TINYINT PRIMARY KEY IDENTITY (1,1),
 nomeTipoHabilidade VARCHAR (25) UNIQUE
 );
 GO
 
-CREATE TABLE HABILIDADE (
+CREATE TABLE habilidade (
 idHabilidade TINYINT PRIMARY KEY IDENTITY (1,1),
-nomeHabilidade VARCHAR (50) NOT NULL UNIQUE
+nomeHabilidade VARCHAR (50) NOT NULL UNIQUE,
+idTipoHabilidade TINYINT FOREIGN KEY REFERENCES tipoHabilidade (idTipoHabilidade)
 );
 GO
  
-ALTER TABLE HABILIDADE
-ADD idTipoHabilidade TINYINT FOREIGN KEY REFERENCES TIPO_HABILIDADE (idTipoHabilidade);
-GO
-
-CREATE TABLE CLASSE_HABILIDADE(
-idHabilidade TINYINT FOREIGN KEY REFERENCES HABILIDADE (idHabilidade),
-idClasse TINYINT FOREIGN KEY REFERENCES CLASSE (idClasse)
+CREATE TABLE classeHabilidade(
+idHabilidade TINYINT FOREIGN KEY REFERENCES habilidade (idHabilidade),
+idClasse TINYINT FOREIGN KEY REFERENCES classe (idClasse)
 );
 GO
 
-CREATE TABLE PERSONAGEM(
+CREATE TABLE personagem(
 nomePersonagem VARCHAR (6) UNIQUE NOT NULL,
 idPersonagem  TINYINT PRIMARY KEY IDENTITY (1,1),
-idClasse TINYINT FOREIGN KEY REFERENCES CLASSE (idClasse),
+idClasse TINYINT FOREIGN KEY REFERENCES classe (idClasse),
 capacidadeVida INT,
 capacidadeMana INT,
 dataAtualizacao DATE,
@@ -47,45 +63,53 @@ GO
 
 ------------------------------------------------------------DML----------------------------------------------------------------
 
-INSERT CLASSE (nomeClasse)
+INSERT classe (nomeClasse)
 VALUES ('BÁRBARO'),('CRUZADO'),('CAÇADORA DE DEMÔNIOS'),('MONGE'),('NECROMANTE'),('FEITICEIRO'),('ARCANISTA');
 GO
 
-INSERT TIPO_HABILIDADE (nomeTipoHabilidade)
+INSERT tipoHabilidade(nomeTipoHabilidade)
 VALUES ('ATAQUE'), ('DEFESA'), ('CURA'), ('MAGIA');
 GO
 
 
- INSERT HABILIDADE (nomeHabilidade, idTipoHabilidade)
+ INSERT habilidade (nomeHabilidade, idTipoHabilidade)
  VALUES ('Lança Mortal',1), ('Escudo Supremo',2),('Recuperar Vida',3);
  GO
 
-INSERT CLASSE_HABILIDADE(idClasse, idHabilidade)
+INSERT classeHabilidade (idClasse, idHabilidade)
 VALUES (1,1),(2,2),(3,1),(4,3),(5,NULL),(6,3),(7,NULL),(1,2),(4,2);
 GO
 
 
-INSERT PERSONAGEM (idClasse, nomePersonagem,capacidadeVida,capacidadeMana,dataAtualizacao,dataCriacao)
+INSERT personagem (idClasse, nomePersonagem,capacidadeVida,capacidadeMana,dataAtualizacao,dataCriacao)
 VALUES (1,'DeuBug','100','80',GETDATE(),'18/01/2019'), (4,'BitBug','70','100',GETDATE(),'17/03/2016'), (7,'Fer8','75','60',GETDATE(),'18/03/2018');
 GO
 
+INSERT tipoUsuario (tipoUsuario)
+VALUES ('ADM'),('JOGADOR');
+GO
+
+INSERT usuario(nomeUsuario, email, senha, idTipoUsuario)
+VALUES ('Saulove', 'saulo@email.com','saulo123',1), ('nay_ara777', 'nay777@email.com', 'nayara123',2),('lucas_nepomusceninho','lucas@email.com', 'lucas123',2)
 --Atualizar o nome do personagem Fer8 para Fer7;
-UPDATE PERSONAGEM SET nomePersonagem = 'Fer7' WHERE idPersonagem = 3
+UPDATE personagem SET nomePersonagem = 'Fer7' WHERE idPersonagem = 3
 --Atualizar o nome da classe de Necromante para Necromancer;
-UPDATE CLASSE SET nomeClasse = 'NECROMANCER' WHERE idClasse = 5
+UPDATE classe SET nomeClasse = 'NECROMANCER' WHERE idClasse = 5
 
 ------------------------------------------------------------DQL----------------------------------------------------------------
 
 --Selecionar todos as classes;
-SELECT * FROM CLASSE
+SELECT * FROM classe
 --Selecionar todos os tipos de habilidades;
-SELECT * FROM TIPO_HABILIDADE
+SELECT * FROM tipoHabilidade
 --Selecionar todas as habilidades;
-SELECT * FROM HABILIDADE
-SELECT * FROM CLASSE_HABILIDADE
+SELECT * FROM habilidade
+SELECT * FROM classeHabilidade
 --Selecionar todos os personagens;
-SELECT * FROM PERSONAGEM
+SELECT * FROM personagem
 
+SELECT * FROM tipoUsuario
+SELECT * FROM usuario
 
 --Selecionar somente o nome das classes;
 SELECT nomeClasse AS Classe FROM CLASSE;
@@ -101,14 +125,14 @@ FROM  HABILIDADE
 ORDER BY  idHabilidade ASC
 
 --Selecionar todas as habilidades e a quais tipos de habilidades elas fazem parte;
-SELECT nomeHabilidade AS 'Habilidade', nomeTipoHabilidade AS 'Tipo da habilidade' FROM HABILIDADE
-INNER JOIN TIPO_HABILIDADE
-ON TIPO_HABILIDADE.idTipoHabilidade = HABILIDADE.idTipoHabilidade
+SELECT nomeHabilidade AS 'Habilidade', nomeTipoHabilidade AS 'Tipo da habilidade' FROM habilidade
+INNER JOIN tipoHabilidade
+ON tipoHabilidade.idTipoHabilidade = habilidade.idTipoHabilidade
 
 --Selecionar todos os personagens e suas respectivas classes;
-SELECT nomePersonagem AS 'Personagem',nomeClasse as 'Classe' FROM PERSONAGEM
+SELECT nomePersonagem AS 'Personagem',nomeClasse as 'Classe' FROM personagem
 INNER JOIN CLASSE
-ON CLASSE.idClasse = PERSONAGEM.idClasse
+ON classe.idClasse = personagem.idClasse
 
 --Selecionar todos os personagens e as classes (mesmo que elas não tenham correspondência em personagens);
 SELECT nomePersonagem AS 'Personagem',nomeClasse AS 'Classe' FROM PERSONAGEM
@@ -117,26 +141,26 @@ ON CLASSE.idClasse = PERSONAGEM.idClasse
 
 --Selecionar todas as classes e suas respectivas habilidades;
 SELECT nomeClasse AS Classe, nomeHabilidade AS Habilidade FROM CLASSE
-INNER JOIN CLASSE_HABILIDADE
-ON CLASSE.idClasse  = CLASSE_HABILIDADE.idClasse
+INNER JOIN classeHabilidade
+ON CLASSE.idClasse  = classeHabilidade.idClasse
 INNER JOIN HABILIDADE
-ON HABILIDADE.idHabilidade = CLASSE_HABILIDADE.idHabilidade;
+ON HABILIDADE.idHabilidade = classeHabilidade.idHabilidade;
 GO
 
 --Selecionar todas as habilidades e suas classes (somente as que possuem correspondência);
 SELECT nomeClasse AS Classe, nomeHabilidade AS Habilidade FROM CLASSE
-RIGHT JOIN CLASSE_HABILIDADE
-ON classe.idClasse  = CLASSE_HABILIDADE.idClasse
+RIGHT JOIN classeHabilidade
+ON classe.idClasse  = classeHabilidade.idClasse
 RIGHT JOIN HABILIDADE
-ON HABILIDADE.idHabilidade = CLASSE_HABILIDADE.idHabilidade;
+ON HABILIDADE.idHabilidade = classeHabilidade.idHabilidade;
 GO
 
 --Selecionar todas as habilidades e suas classes (mesmo que elas não tenham correspondência).
-SELECT nomeClasse AS Classe, nomeHabilidade AS Habilidade FROM CLASSE_HABILIDADE
+SELECT nomeClasse AS Classe, nomeHabilidade AS Habilidade FROM classeHabilidade
 LEFT JOIN CLASSE
-ON CLASSE.idClasse  = CLASSE_HABILIDADE.idClasse
+ON CLASSE.idClasse  = classeHabilidade.idClasse
 LEFT JOIN HABILIDADE
-ON HABILIDADE.idHabilidade = CLASSE_HABILIDADE.idHabilidade;
+ON HABILIDADE.idHabilidade = classeHabilidade.idHabilidade;
 GO
 
 
