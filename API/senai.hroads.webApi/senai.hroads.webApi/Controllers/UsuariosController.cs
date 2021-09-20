3806bc186ea0929dbb4c22fd627b518f376d575a
Nayara Nogueira Escudeiro do Nascimento <nanaescudeiro2015@gmail.com>
2021-09-20 18:20:20 -0300

    jwt e swagger feitos, falta teste

--- API/senai.hroads.webApi/senai.hroads.webApi/Controllers/UsuariosController.cs
+++ API/senai.hroads.webApi/senai.hroads.webApi/Controllers/UsuariosController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using senai.hroads.webApi_.Domains;
 using senai.hroads.webApi_.Interfaces;
 using senai.hroads.webApi_.Repositories;
+using senai.hroads.webApi_.ViewsModels;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace senai.hroads.webApi_.Controllers
@@ -64,6 +68,48 @@ namespace senai.hroads.webApi_.Controllers
 
             return StatusCode(204);
 
+        }
+
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                Usuario usuarioBuscado = _usuarioRepository.Login(login.Email, login.Senha);
+
+                if (usuarioBuscado == null)
+                {
+                    return NotFound("E-mail ou senha inválidos");
+                }
+
+                var minhasClaims = new[]
+                {
+                    new Claim (JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                    new Claim (JwtRegisteredClaimNames.Jti, usuarioBuscado.IdUsuario.ToString()),
+                    new Claim(ClaimTypes.Role, usuarioBuscado.IdTipoUsuario.ToString())
+                };
+
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("hroads-chave-autenticacao"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var meuToken = new JwtSecurityToken(
+                        issuer: "hroads.webAPI",
+                        audience: "hroads.webAPI",
+                        claims: minhasClaims,
+                        expires: DateTime.Now.AddMinutes(30),
+                        signingCredentials: creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(meuToken)
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
--- API/senai.hroads.webApi/senai.hroads.webApi/Domains/Usuario.cs
+++ API/senai.hroads.webApi/senai.hroads.webApi/Domains/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -9,7 +10,12 @@ namespace senai.hroads.webApi_.Domains
     {
         public short IdUsuario { get; set; }
         public string NomeUsuario { get; set; }
+
+        [Required(ErrorMessage = "O campo e-mail é obrigatorio!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatorio!")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "A senha deve ter entre 3 a 10 caracteres!")]
         public string Senha { get; set; }
         public short? IdTipoUsuario { get; set; }
 
